c53f508bcfeb081d2f69999317a10458b6c8404f
Paulo Borges <poolborges@hotmail.com>
2020-10-09 18:57:10 +0100

    Update docker entrypoint

--- osVodigiWeb6x-docker/scripts/entrypoint.sh
+++ osVodigiWeb6x-docker/scripts/entrypoint.sh
@@ -16,8 +16,11 @@ echo Populate Vodigi 5.0 database...
 echo Creating VodigiLog 5.0 database...
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i "./sql/VodigiLogs 5.0 Master DB Scripts.sql"
 
-echo Upagrade Vodigi database 5.0 to 5.5
+echo Upgrade Vodigi database 5.0 to 5.5
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i "./sql/Vodigi 5.0 to 5.5 - DB Upgrade Scripts.sql"
 
-echo Upagrade Vodigi database 5.5 to 6.0
+echo Upgrade Vodigi database 5.5 to 6.0
 /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i "./sql/Vodigi 5.5 to 6.0 - DB Upgrade Scripts.sql"
+
+echo Vodigi 6.0 - Initial Account and User Admin:Admin
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i "./sql/Vodigi 6.0 - Initial Account Scripts.sql"
